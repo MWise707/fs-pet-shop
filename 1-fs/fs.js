8b293f7879b900c6c439eeff5f2250fdb09121b4
MWise707 <markezrawiseman@gmail.com>
2023-12-06 10:15:43 -0800

    Add subcommands for read and create

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import process from "process";
 
-console.error(`Usage: node fs.js [read | create | update | destroy]`);
+// console.error(`Usage: node fs.js [read | create | update | destroy]`);
 
 if (!process.argv[2]) {
   console.error(`Usage: node fs.js [read | create | update | destroy]`);
@@ -12,16 +12,27 @@ if (process.argv[2] === "read") {
   fs.readFile("../pets.json", "utf8", function (error, data) {
     if (error) {
       console.log(error);
+    } else if (process.argv[3] !== undefined) {  // define error state for subcommand 
+      let petShop = JSON.parse(data);
+      let index = parseInt(process.argv[3]);
+      // if index does not exist -- display // 
+      if (petShop[index] === undefined) {
+        console.log('Usage: node fs.js read INDEX')
+      } else {
+        console.log(petShop[index])
+      }
+      // otherwise do the things
+
     } else {
-      console.log(JSON.parse(data));
+      console.log(petShop);
     }
   });
 }
 
 if (process.argv[2] === "create") {
   fs.readFile("../pets.json", "utf8", function (error, data) {
-    if (!process.argv.length === 6) {
-      console.log(error);
+    if (process.argv.length !== 6) {
+      console.log("Usage: node fs.js create AGE KIND NAME");
     } else {
       let petShop = JSON.parse(data);
       let newPet = {
