5084b7a161272c0e8d11358267f53e0053f109b9
MWise707 <markezrawiseman@gmail.com>
2023-12-07 15:31:10 -0800

    Part 3 complete

--- 2-http/http.js
+++ 2-http/http.js
@@ -4,53 +4,44 @@ const http = require("http");
 // const { runInNewContext } = require("vm");
 const port = process.env.PORT || 8000;
 const fs = require("fs");
+const regex = /^\/pets\/(\d+)$/; // matches url where index is positive integer
 
 const server = http.createServer(function (req, res) {
   console.log(req.url);
-  const pets = req.url.split("/");
-  console.log("Checking index before changing to number: ", pets[2]);
-  console.log("Type of index before changing: ", typeof pets[2]);
-  // console.log("length of index: ", pets[2].length);
-  const index = parseInt(pets[2]);
-  console.log("Index line 13 is:", typeof index);
-  console.log(isNaN(index));
+
+  const match = req.url.match(regex);
+  if (!match) {
+    console.log("invalid URL!");
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("not found");
+  }
+
+  const index = parseInt(match[1]);
 
   // use this index in the pets.json to read that specific pet data in that object
   fs.readFile("../pets.json", "utf8", function (error, data) {
     if (error) {
-      console.log("This is my internal server error", error);
-      res.statusCode = 500;
-      res.end(error);
-    } else if (isNaN(index) && req.method === "GET") {
-      // check if pets[2] === undefined
-      if (pets[2] !== undefined) {
-        console.log("Did not have valid index");
-        res.setHeader = ("Content-Type", "text/plain");
-        res.end("Not Found");
-      } else {
-        res.statusCode = 200;
-        res.setHeader = ("Content-Type", "application/JSON");
-        console.log(JSON.parse(data));
-        res.end(data);
-      }
-    } else if (index >= 0 && req.method === "GET") {
-      if (JSON.parse(data)[index] === undefined) {
-        // if index at petObj is undefined
-        console.log("Pet does not exist");
-        res.statusCode = 404;
-        res.setHeader = ("Content-Type", "text/plain");
-        res.end("Not Found");
-      } else {
-        console.log(JSON.parse(data)[index]);
-        res.statusCode = 200;
-        res.setHeader = ("Content-Type", "application/JSON");
-        res.end(JSON.stringify(JSON.parse(data)[index]));
-      }
-    } else if (index < 0 && req.method === "GET") {
-      console.log("Negative index sent");
+      console.log("Error!");
       res.statusCode = 404;
-      res.setHeader = ("Content-Type", "text/plain");
-      res.end("Not Found");
+      res.setHeader("Content-Type", "text/plain");
+      res.end("not found");
+    } else if (isNaN(index) || index < 0) {
+      // not fo
+      console.log("Error!");
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("not found");
+    } else if (JSON.parse(data)[index] === undefined) {
+      console.log("Error!");
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("not found");
+    } else {
+      console.log(JSON.parse(data)[index]);
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(JSON.parse(data)[index]));
     }
   });
 });
