fe62b088f5147194b833d8e9c448ca89b2fd07dd
MWise707 <markezrawiseman@gmail.com>
2023-12-07 06:33:23 -0800

    .

---
+++ 2-http/http.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const http = require("http");
+// const { runInNewContext } = require("vm");
+const port = process.env.PORT || 8000;
+const fs = require("fs");
+
+const server = http.createServer(function (req, res) {
+  console.log(req.url);
+  const pets = req.url.split("/");
+  const index = parseInt(pets[2]);
+  console.log("Index line 13 is:", typeof index);
+  console.log(isNaN(index));
+
+  // use this index in the pets.json to read that specific pet data in that object
+  fs.readFile("../pets.json", "utf8", function (error, data) {
+    if (error) {
+      console.log("This is my internal server error", error);
+      res.statusCode = 500;
+      res.end(error);
+    } else if (isNaN(index) && req.method === "GET") {
+      res.statusCode = 200;
+      res.setHeader = ("Content-Type", "application/JSON");
+      console.log(JSON.parse(data));
+      res.end(data);
+    } else if (index >= 0 && req.method === "GET") {
+      if (JSON.parse(data)[index] === undefined) {
+        // if index at petObj is undefined
+        console.log("Pet does not exist");
+        res.statusCode = 404;
+        res.setHeader = ("Content-Type", "text/plain");
+        res.end("Not Found");
+      } else {
+        console.log(JSON.parse(data)[index]);
+        res.statusCode = 200;
+        res.setHeader = ("Content-Type", "application/JSON");
+        res.end(JSON.stringify(JSON.parse(data)[index]));
+      }
+    } else if (index < 0 && req.method === "GET") {
+      console.log("Negative index sent");
+      res.statusCode = 404;
+      res.setHeader = ("Content-Type", "text/plain");
+      res.end("Not Found");
+    }
+  });
+});
+
+server.listen(port, function () {
+  console.log("Listening on port", port);
+});
